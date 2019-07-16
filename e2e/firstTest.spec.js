e3ec9916b366d2411c9af72dc2b952cd4a067560
Brooke Mitchell <brooke.mitchell@vetradar.com>
2019-07-16 20:52:01 +1200

    revert test changes

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,59 +1,48 @@
-const {
-  reloadApp,
-  getAppUrl,
-  blacklistLiveReloadUrl
-  // blacklistCmdlineFormat
-} = require('detox-expo-helpers');
+const { reloadApp, getAppUrl,blacklistLiveReloadUrl,blacklistCmdlineFormat } = require('detox-expo-helpers')
+
 
 describe('Example', () => {
   it('should load the app', async () => {
-    console.log('Lauching using reloadApp');
-    const url = await getAppUrl();
-    await reloadApp({ urlBlacklist: [url + '/onchange'] });
-    console.log('App launched');
-    await expect(
-      element(by.label('Open up App.js to start working on your app!'))
-    ).toBeVisible();
+    console.log("Lauching using reloadApp")
+    await reloadApp();
+    console.log("App launched")
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
   });
 
-  it.skip('should load the app with the url', async () => {
-    console.log('Lauching expo with URL');
+  it('should load the app with the url', async () => {
+    console.log("Lauching expo with URL")
     const url = await getAppUrl();
-    const blacklist = await blacklistCmdlineFormat();
-    console.log('Blacklist: ', blacklist);
+    const blacklist = await blacklistCmdlineFormat()
+    console.log("Blacklist: ", blacklist)
     await device.launchApp({
       newInstance: true,
       sourceApp: 'host.exp.exponent',
       url,
-      launchArgs: {
-        EXKernelDisableNuxDefaultsKey: true,
+      launchArgs: { 
+        EXKernelDisableNuxDefaultsKey: true, 
         detoxPrintBusyIdleResources: 'YES',
         detoxURLBlacklistRegex: blacklist
-      }
-    });
-    console.log('App launched');
-    await expect(
-      element(by.label('Open up App.js to start working on your app!'))
-    ).toBeVisible();
+      },
+    })
+    console.log("App launched")
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
   });
 
-  it.skip('should open the app URL', async () => {
-    console.log('Lauching expo');
+ it('should open the app URL', async () => {
+    console.log("Lauching expo")
     await device.launchApp({
       newInstance: true,
       sourceApp: 'host.exp.exponent',
-      launchArgs: {
-        EXKernelDisableNuxDefaultsKey: true,
-        detoxPrintBusyIdleResources: 'YES'
-      }
-    });
-    await blacklistLiveReloadUrl();
+      launchArgs: { 
+        EXKernelDisableNuxDefaultsKey: true, 
+        detoxPrintBusyIdleResources: 'YES',
+      },
+    })
+    await blacklistLiveReloadUrl()
     const url = await getAppUrl();
-    console.log('Opening app URL:', url);
-    await device.openURL({ url });
-    console.log('App launched');
-    await expect(
-      element(by.label('Open up App.js to start working on your app!'))
-    ).toBeVisible();
+    console.log("Opening app URL:", url);
+    await device.openURL({url})
+    console.log("App launched")
+    await expect(element(by.label('Open up App.js to start working on your app!'))).toBeVisible();
   });
 });
